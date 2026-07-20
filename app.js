b65c257963b303931c06589a43a01c5d9c174d37
davideloi55-prog <davideloi55@gmail.com>
2026-07-20 16:33:28 +0100

    Add image upload

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-console.log("BO2 Emblem Generator iniciado!");
-
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Página carregada com sucesso.");
+
+    const imageButton = document.getElementById("imageButton");
+    const imageInput = document.getElementById("imageInput");
+    const preview = document.getElementById("preview");
+
+    const generateButton = document.getElementById("generateButton");
+    const result = document.getElementById("result");
+
+    // Abrir a galeria quando clicares no botão
+    imageButton.addEventListener("click", () => {
+        imageInput.click();
+    });
+
+    // Mostrar a imagem escolhida
+    imageInput.addEventListener("change", (event) => {
+
+        const file = event.target.files[0];
+
+        if (!file) return;
+
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+
+            preview.src = e.target.result;
+            preview.style.display = "block";
+
+            result.innerHTML = "✅ Imagem carregada com sucesso!";
+
+        };
+
+        reader.readAsDataURL(file);
+
+    });
+
+    // Botão Gerar (temporário)
+    generateButton.addEventListener("click", () => {
+
+        result.innerHTML =
+        "🚧 O gerador BO2 será implementado nas próximas versões.";
+
+    });
+
 });

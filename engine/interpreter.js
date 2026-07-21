5977cef33da475e2490040e461021549ee46e460
davideloi55-prog <davideloi55@gmail.com>
2026-07-21 19:58:34 +0100

    Add Interpreter object with interpret method

--- engine/interpreter.js
+++ engine/interpreter.js
@@ -1,1 +1,21 @@
+const Interpreter = {
 
+    interpret(text){
+
+        Debug.log("🧠 Interpreter iniciado");
+
+        return {
+
+            quality: "balanced",
+
+            removeBackground: false,
+
+            preserveColors: true,
+
+            preserveText: true
+
+        };
+
+    }
+
+};

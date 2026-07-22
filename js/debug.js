9c11b91e821d49f749b62bdf3da11885c3956772
davideloi55-prog <davideloi55@gmail.com>
2026-07-22 09:21:28 +0100

    Refactor debug initialization into Debug object

--- js/debug.js
+++ js/debug.js
@@ -10,12 +10,33 @@ const Debug = {
 
     },
 
+    const Debug = {
+
+    panel: null,
+
+    init() {
+
+        this.panel = document.getElementById("debugPanel");
+
+        this.log("🟢 Debug iniciado");
+
+    },
+
     log(message) {
 
         if (!this.panel) return;
 
         this.panel.innerHTML += "<br>" + message;
 
+    }
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    Debug.init();
+
+});
     }
 
 };
